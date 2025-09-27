cf37b352a9120fb370ffae6cfe6753c3e15c43fb
ngocanh204 <dongocanh131104@gmail.com>
2025-09-27 12:30:02 +0700

    Update Practice_02.sql

--- Practice_02.sql
+++ Practice_02.sql
@@ -44,3 +44,32 @@ GROUP BY card_name
 ORDER BY MAX(issued_amount)-MIN(issued_amount) DESC
 
 /* Bài tập 8: https://datalemur.com/questions/non-profitable-drugs */
+SELECT manufacturer,
+count(drug) AS drug_count,
+SUM(cogs-total_sales) AS total_loss
+FROM pharmacy_sales
+WHERE cogs>total_sales 
+GROUP BY manufacturer
+ORDER BY total_loss DESC;
+
+/* Bài tập 9: https://leetcode.com/problems/not-boring-movies/?envType=study-plan-v2&envId=top-sql-50 */
+SELECT *
+FROM cinema
+WHERE ID%2 !=0 AND description !='boring'
+ORDER BY rating desc
+
+/* Bài tập 10: https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/?envType=study-plan-v2&envId=top-sql-50 */
+SELECT teacher_id,
+count(DISTINCT subject_id) AS cnt
+FROM teacher
+GROUP BY teacher_id
+
+/* Bài tập 11: https://leetcode.com/problems/find-followers-count/?envType=study-plan-v2&envId=top-sql-50 */
+SELECT user_id ,
+count(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id 
+ORDER BY USER_ID ASC 
+
+/* Bài tập 12: https://leetcode.com/problems/classes-more-than-5-students/?envType=study-plan-v2&envId=top-sql-50 */
+--Page Not Found
