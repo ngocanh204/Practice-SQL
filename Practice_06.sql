4d18a14f83333706aadc5c8f9b1528d479a19304
ngocanh204 <dongocanh131104@gmail.com>
2025-10-16 00:42:03 +0700

    Update Practice_06.sql

--- Practice_06.sql
+++ Practice_06.sql
@@ -23,3 +23,40 @@ ORDER BY total_spend desc
 LIMIT 4)
 SELECT * FROM category_total_spend
 ORDER BY category asc
+
+
+/* Bài tập 3: https://datalemur.com/questions/frequent-callers */ 
+with policy_holder_table as(
+SELECT policy_holder_id,
+COUNT(case_id)
+FROM callers
+GROUP BY policy_holder_id
+HAVING COUNT(case_id)>=3)
+SELECT 
+count(policy_holder_id) AS policy_holder_count
+FROM policy_holder_table
+
+/* Bài tập 4:  https://datalemur.com/questions/sql-page-with-no-likes */
+SELECT a.page_id FROM pages as A 
+LEFT JOIN page_likes as B 
+ON a.page_id=b.page_id
+WHERE user_id is NULL
+ORDER BY page_id
+
+/* Bài tập 5: https://datalemur.com/questions/user-retention */
+SELECT
+ EXTRACT(month from event_date) AS mth,
+ COUNT(DISTINCT(user_id)) AS monthly_active_user
+FROM user_actions 
+WHERE user_id IN (
+-- Give me the list of all distinct users in the month of June 2022
+  SELECT 
+  	DISTINCT(user_id)
+  FROM user_actions
+  WHERE EXTRACT(month from event_date) = 6 AND EXTRACT(year from event_date) = 2022
+  )
+AND EXTRACT(month from event_date) = 7 AND EXTRACT(year from event_date) = 2022
+GROUP BY mth;
+
+
+
