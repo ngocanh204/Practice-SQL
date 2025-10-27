8d05ab8d75c996f8eed0638e5765925028b45321
ngocanh204 <dongocanh131104@gmail.com>
2025-10-28 01:45:40 +0700

    Update Practice_06.sql

--- Practice_06.sql
+++ Practice_06.sql
@@ -58,5 +58,62 @@ WHERE user_id IN (
 AND EXTRACT(month from event_date) = 7 AND EXTRACT(year from event_date) = 2022
 GROUP BY mth;
 
+/* Bài 6: https://leetcode.com/problems/monthly-transactions-i/?envType=study-plan-v2&envId=top-sql-50 */
+
+SELECT 
+    TO_CHAR(trans_date, 'YYYY-MM') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(amount) AS trans_total_amount,
+    COUNT(CASE WHEN state = 'approved' THEN 1 END) AS approved_count,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY TO_CHAR(trans_date, 'YYYY-MM'), country;
 
 
+/* Bài 7:https://leetcode.com/problems/product-sales-analysis-iii/?envType=study-plan-v2&envId=top-sql-50 */
+
+SELECT 
+    product_id,
+    year AS first_year,
+    quantity,
+    price
+FROM Sales
+WHERE (product_id, year) IN (
+    SELECT product_id, MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+);
+
+
+/* Bài tập 8 :https://leetcode.com/problems/customers-who-bought-all-products/?envType=study-plan-v2&envId=top-sql-50 */
+
+SELECT CUSTOMER_ID 
+FROM Customer
+group by customer_id 
+having count(distinct product_key )=(
+    SELECT count(distinct product_key) from Product 
+    );
+
+/* Bài tập 9: https://leetcode.com/problems/employees-whose-manager-left-the-company/?envType=study-plan-v2&envId=top-sql-50 */
+
+SELECT employee_id
+FROM Employees 
+WHERE salary <30000 AND manager_id not in (
+    SELECT employee_id  FROM Employees 
+)
+
+/* Bài tập 10: https://datalemur.com/questions/duplicate-job-listings */
+
+select count (DISTINCT company_id) AS duplicate_companies
+FROM
+(SELECT 
+company_id,
+title,
+description,
+COUNT(job_id)
+from job_listings
+GROUP BY company_id,title,description
+HAVING COUNT(job_id)>1) AS job_count
+
+/* Bài 11: 
