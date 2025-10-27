32e2afd577784351483ef2d517ba15eb1d305a97
ngocanh204 <dongocanh131104@gmail.com>
2025-10-28 02:16:55 +0700

    Update Practice_06.sql

--- Practice_06.sql
+++ Practice_06.sql
@@ -116,4 +116,24 @@ from job_listings
 GROUP BY company_id,title,description
 HAVING COUNT(job_id)>1) AS job_count
 
-/* Bài 11: 
+/* Bài 11: https://leetcode.com/problems/movie-rating/description/?envType=study-plan-v2&envId=top-sql-50 */
+
+(
+  SELECT name AS results
+  FROM Users u
+  JOIN MovieRating mr ON u.user_id = mr.user_id
+  GROUP BY u.user_id, u.name
+  ORDER BY COUNT(*) DESC, u.name ASC
+  LIMIT 1
+)
+UNION ALL
+(
+  SELECT title AS results
+  FROM Movies m
+  JOIN MovieRating mr ON m.movie_id = mr.movie_id
+  WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+  GROUP BY m.movie_id, m.title
+  ORDER BY AVG(mr.rating) DESC, m.title ASC
+  LIMIT 1
+);
+
